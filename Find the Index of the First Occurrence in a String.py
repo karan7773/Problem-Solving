a004b5fdc15dee022fc784c13934a522b2ab11d0
karan7773 <karan321077@gmail.com>
2023-12-12 13:28:35 +0530

    find the index of the First Occurrence in a string

---
+++ Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,7 @@
+def strStr(haystack, needle):
+    if needle not in haystack:
+        return -1
+    if needle in haystack:
+        return haystack.index(needle)
+
+print(strStr("leetcode","leet"))
